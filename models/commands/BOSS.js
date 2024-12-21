247d60d4cf61f1f98baf788072f557282b97e7aa
Shaanbot1234 <b8465391@gmail.com>
2024-12-21 22:14:50 +0500

    Create BOSS.js

---
+++ models/commands/BOSS.js
@@ -0,0 +1,44 @@
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
+
+module.exports.config = {
+    name: "boss",
+    version: "1.0.1",
+    hasPermssion: 0,
+    credits: "SHANKAR SUMAN",
+    description: "no prefix",
+    usePrefix: false,
+    commandCategory: "No command marks needed",
+    usages: "Yo Yo",
+    cooldowns: 5,
+};
+
+const gif = "https://i.imgur.com/ZOZv3qU.jpeg";
+const message = "● ======= 𝐇𝐄𝐋𝐋𝐎 𝐁𝐀𝐁𝐘 ======= ●                                                          ☟  ========== ☟ ==========  ☟.                                                         ●============================●                              𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐀𝐚̃𝐃𝐢   𝐁𝐚𝐁𝐮";
+
+module.exports.handleEvent = async function({ api, event, client, Users, __GLOBAL }) {
+    var { threadID, messageID } = event;
+    const lowerCaseMessage = event.body.toLowerCase();
+
+    if (lowerCaseMessage.startsWith("shan") || 
+        lowerCaseMessage.startsWith("SHAAN") || 
+        lowerCaseMessage.startsWith("Shaan")) { 
+
+        const downloadPath = path.join(__dirname, 'Boss-Jpg-Images.jpg');
+
+        // Download image from Imgur
+        request(gif).pipe(fs.createWriteStream(downloadPath)).on('close', () => {
+            var msg = {
+                body: message,
+                attachment: fs.createReadStream(downloadPath)
+            };
+            api.sendMessage(msg, threadID, messageID);
+            api.setMessageReaction("😘", event.messageID, (err) => {}, true);
+        });
+    }
+}
+
+module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
